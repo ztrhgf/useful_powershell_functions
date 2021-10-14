d1e17f8a1d8930651ae29ae0a109a71452e6ada8
ondrejs4 <ondrejs4@kentico.com>
2021-10-14 13:22:57 +0200

    fix mutliple GUID issue

--- INTUNE/Invoke-MDMReenrollment.ps1
+++ INTUNE/Invoke-MDMReenrollment.ps1
@@ -69,34 +69,38 @@
             $EnrollmentGUID = Get-ScheduledTask | Where-Object { $_.TaskPath -like "*Microsoft*Windows*EnterpriseMgmt\*" } | Select-Object -ExpandProperty TaskPath -Unique | Where-Object { $_ -like "*-*-*" } | Split-Path -Leaf
 
             # Start cleanup process
-            if (![string]::IsNullOrEmpty($EnrollmentGUID)) {
-                Write-Host "Current enrollment GUID detected as $([string]$EnrollmentGUID)"
+            if ($EnrollmentGUID) {
+                $EnrollmentGUID | % {
+                    $GUID = $_
 
-                # Stop Intune Management Exention Agent and CCM Agent services
-                Write-Host "Stopping MDM services"
-                if (Get-Service -Name IntuneManagementExtension -ErrorAction SilentlyContinue) {
-                    Write-Host " - Stopping IntuneManagementExtension service..."
-                    Stop-Service -Name IntuneManagementExtension
-                }
-                if (Get-Service -Name CCMExec -ErrorAction SilentlyContinue) {
-                    Write-Host " - Stopping CCMExec service..."
-                    Stop-Service -Name CCMExec
-                }
+                    Write-Host "Current enrollment GUID detected as $GUID"
 
-                # Remove task scheduler entries
-                Write-Host "Removing task scheduler Enterprise Management entries for GUID - $([string]$EnrollmentGUID)"
-                Get-ScheduledTask | Where-Object { $_.Taskpath -match $EnrollmentGUID } | Unregister-ScheduledTask -Confirm:$false
-                # delete also parent folder
-                Remove-Item -Path "$env:WINDIR\System32\Tasks\Microsoft\Windows\EnterpriseMgmt\$EnrollmentGUID" -Force
+                    # Stop Intune Management Exention Agent and CCM Agent services
+                    Write-Host "Stopping MDM services"
+                    if (Get-Service -Name IntuneManagementExtension -ErrorAction SilentlyContinue) {
+                        Write-Host " - Stopping IntuneManagementExtension service..."
+                        Stop-Service -Name IntuneManagementExtension
+                    }
+                    if (Get-Service -Name CCMExec -ErrorAction SilentlyContinue) {
+                        Write-Host " - Stopping CCMExec service..."
+                        Stop-Service -Name CCMExec
+                    }
 
-                $RegistryKeys = "HKLM:\SOFTWARE\Microsoft\Enrollments", "HKLM:\SOFTWARE\Microsoft\Enrollments\Status", "HKLM:\SOFTWARE\Microsoft\EnterpriseResourceManager\Tracked", "HKLM:\SOFTWARE\Microsoft\PolicyManager\AdmxInstalled", "HKLM:\SOFTWARE\Microsoft\PolicyManager\Providers", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Accounts", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Logger", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Sessions"
-                foreach ($Key in $RegistryKeys) {
-                    Write-Host "Processing registry key $Key"
-                    # Remove registry entries
-                    if (Test-Path -Path $Key) {
-                        # Search for and remove keys with matching GUID
-                        Write-Host " - GUID entry found in $Key. Removing..."
-                        Get-ChildItem -Path $Key | Where-Object { $_.Name -match $EnrollmentGUID } | Remove-Item -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
+                    # Remove task scheduler entries
+                    Write-Host "Removing task scheduler Enterprise Management entries for GUID - $GUID"
+                    Get-ScheduledTask | Where-Object { $_.Taskpath -match $GUID } | Unregister-ScheduledTask -Confirm:$false
+                    # delete also parent folder
+                    Remove-Item -Path "$env:WINDIR\System32\Tasks\Microsoft\Windows\EnterpriseMgmt\$GUID" -Force
+
+                    $RegistryKeys = "HKLM:\SOFTWARE\Microsoft\Enrollments", "HKLM:\SOFTWARE\Microsoft\Enrollments\Status", "HKLM:\SOFTWARE\Microsoft\EnterpriseResourceManager\Tracked", "HKLM:\SOFTWARE\Microsoft\PolicyManager\AdmxInstalled", "HKLM:\SOFTWARE\Microsoft\PolicyManager\Providers", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Accounts", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Logger", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Sessions"
+                    foreach ($Key in $RegistryKeys) {
+                        Write-Host "Processing registry key $Key"
+                        # Remove registry entries
+                        if (Test-Path -Path $Key) {
+                            # Search for and remove keys with matching GUID
+                            Write-Host " - GUID entry found in $Key. Removing..."
+                            Get-ChildItem -Path $Key | Where-Object { $_.Name -match $GUID } | Remove-Item -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
+                        }
                     }
                 }
 
